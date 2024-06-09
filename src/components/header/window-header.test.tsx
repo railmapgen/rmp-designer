69a73a89d4be5d8b54b9e70048b9789518ff2887
5+1 <59787082+langonginc@users.noreply.github.com>
2024-06-09 14:04:52 +0800

    Update test

--- src/components/header/window-header.test.tsx
+++ src/components/header/window-header.test.tsx
@@ -6,6 +6,6 @@ describe('WindowHeader', () => {
     it('Can render window header', () => {
         render(<WindowHeader />);
 
-        expect(screen.getByRole('heading').textContent).toContain('RMP Style Generator');
+        expect(screen.getByRole('heading').textContent).toContain('RMP Designer');
     });
 });
