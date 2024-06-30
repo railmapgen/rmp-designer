a42690123c1a4f382e62256dc132db5bc87c3d4b
5+1 <59787082+langonginc@users.noreply.github.com>
2024-06-30 19:55:51 +0800

    #2 I18n translation

--- src/components/header/about-modal.tsx
+++ src/components/header/about-modal.tsx
@@ -1,4 +1,6 @@
 import {
+    Avatar,
+    Box,
     Flex,
     Image,
     Modal,
@@ -7,7 +9,10 @@ import {
     ModalContent,
     ModalHeader,
     ModalOverlay,
+    Tag,
+    TagLabel,
     Text,
+    VStack,
 } from '@chakra-ui/react';
 import rmgRuntime from '@railmapgen/rmg-runtime';
 import { useTranslation } from 'react-i18next';
@@ -33,9 +38,33 @@ const AboutModal = (props: { isOpen: boolean; onClose: () => void }) => {
                             </Text>
                             <Text>{appVersion}</Text>
                             <Text />
-                            <Text fontSize="sm">{t('header.about.railmapgen')}</Text>
+                            <Text fontSize="sm">{t('header.about.intro')}</Text>
                         </Flex>
                     </Flex>
+
+                    <Box margin={5}>
+                        <Text fontSize="xl">{t('header.about.intro')}</Text>
+                    </Box>
+
+                    <VStack>
+                        <Tag
+                            size="lg"
+                            w="95%"
+                            onClick={() => window.open('https://github.com/langonginc', '_blank')}
+                            cursor="pointer"
+                        >
+                            <Avatar src="https://github.com/langonginc.png" size="lg" my={2} ml={-1} mr={2} />
+                            <TagLabel display="block" width="100%">
+                                <Text fontSize="lg" fontWeight="bold" mb={1}>
+                                    langonginc
+                                </Text>
+                                <Text fontSize="sm">Live a life you will remember.</Text>
+                                <Text fontSize="sm" align="right" mb={1}>
+                                    --Avicii
+                                </Text>
+                            </TagLabel>
+                        </Tag>
+                    </VStack>
                 </ModalBody>
             </ModalContent>
         </Modal>
